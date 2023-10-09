0300d1a4b47124523bc6bea1e1ac424c1613f719
AfiliadoXP <afiliadoxp7@gmail.com>
2023-10-08 23:35:11 -0300

    first commit AXP

---
+++ src/main/java/com/afiliadoxp/Axp17Application.java
@@ -0,0 +1,13 @@
+package com.afiliadoxp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Axp17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Axp17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/afiliadoxp/Axp17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afiliadoxp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Axp17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
